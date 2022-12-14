604688c50a28c0e934c65d7865de73f9e50032c5
Laerte <laerteakira@gmail.com>
2022-12-14 11:24:38 -0300

    feat: form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { Header } from './components/Header/Header';
 
+import style from './App.module.css';
+import { PlusCircle } from 'phosphor-react';
+
 export function App() {
   return (
-    <Header />
+    <>
+      <Header />
+
+      <div className={style.wrapper}>
+        <form className={style.form}>
+          <input 
+            placeholder='Adicione uma nova tarefa'
+          />
+          <button>
+            Criar
+            <PlusCircle size={'1.25rem'}/>
+          </button>
+        </form>
+      </div>
+    </>
   )
 }
